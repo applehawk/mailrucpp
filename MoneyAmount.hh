c773482999bfcb04da056b1ca7744f90aefa2ade
Pyotr Ermishkin <quasiyoke@gmail.com>
2012-10-11 23:28:44 +0400

    MoneyAmount.hh added.

---
+++ MoneyAmount.hh
@@ -0,0 +1,19 @@
+/* 
+   
+ */
+
+#ifndef _MoneyAmount_hh_
+#define _MoneyAmount_hh_
+
+class MoneyAmount {
+
+  public:
+    Currency currency;
+    Decimal amount;
+
+
+
+};
+
+#endif /* _MoneyAmount_hh_ */
+
