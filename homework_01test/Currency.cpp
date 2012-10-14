a915d24f0b8c4f05de929d92e78cbc31c19e0f65
Vladislav Vasilenko <xyzslash@gmail.com>
2012-10-14 13:16:44 +0400

    Modif MoneyAmount,Currency

--- homework_01test/Currency.cpp
+++
@@ -1,7 +0,0 @@
-#include "Currency.h"
-
-bool operator==( const Currency& left, const Currency& right){
-  return ( (left.getValue() == right.getValue()) && 
-        ( left.getName() == right.getName() ) && 
-        ( left.getSign() == right.getSign() ) );
-}
