237c333ad2b3a78396cddab8b81820785941b197
Ruslan <ruslan@Ruslan-PC.(none)>
2012-11-03 19:48:35 +0400

    Class Person initial commit

---
+++ homework_01/Person.cpp
@@ -0,0 +1,45 @@
+#include "Person.h"
+#include <iostream>
+#include <sstream>
+#include <ctime>
+
+
+time_t Person::encode(std::string date){
+	int day = convert(0, 2, date);
+	int month = convert(3, 5, date);
+	int year = convert(6, 9, date);
+	//std::cout << "Day: " << day<< "  " << "Month: " << month << "  " << "Year: " << year << std::endl;
+
+	time_t rawtime;
+	struct tm * timeinfo;
+
+	timeinfo = localtime ( &rawtime );
+	timeinfo->tm_year = year - 1900;
+	timeinfo->tm_mon = month - 1;
+	timeinfo->tm_mday = day;
+	timeinfo->tm_hour = 0;
+	timeinfo->tm_min = 0;
+	timeinfo->tm_sec = 0;
+	rawtime = mktime (timeinfo);
+	//std::cout << ctime(&rawtime) << std::endl;
+	return rawtime;
+}
+
+int Person::convert(unsigned short b, unsigned short e, std::string& str){
+	int i;
+	std::stringstream ss(str.substr(b,e)); ss >> i;
+	return i;
+}
+
+time_t Person::setCurTime(){
+	//time_t rawtime=time(0);
+	//std::cout << ctime(&rawtime) << std::endl;
+	return time(0);
+}
+
+void Person::printPerson(){
+	std::cout << "Name: "<< m_first_name << std::endl;
+	std::cout << "Second name: "<< m_second_name << std::endl;
+	std::cout << "Date of birth: "<< ctime(&m_birth);
+	std::cout << "Date of creation: "<< ctime(&m_created) << std::endl;
+}
--- homework_01/Person.h
+++ homework_01/Person.h
@@ -1,28 +1,32 @@
-/* 
-   
- */
-
 #ifndef _Person_hh_
 #define _Person_hh_
 
-#include <string>
+#include <iostream>
 
 class Person {
+private:
+    time_t m_created;
+    time_t encode(std::string str);
+    time_t setCurTime();
+    int convert(unsigned short b, unsigned short e, std::string&);
+    Person();
 public:
-	Person( const std::string &first_name, const std::string &second_name ) {
-		m_first_name = first_name;
-		m_second_name = second_name;
+	Person(std::string _first_name, std::string _second_name, std::string _birth)
+	{
+		m_first_name = _first_name;
+		m_second_name = _second_name;
+		m_birth = encode(_birth);
+		m_created = setCurTime();
 	}
-
-	std::string getFullName() {
-		return ( m_first_name + " " + m_second_name );
-	}
+	void changeFirstName(std::string str) {m_first_name = str;}
+	void changeSecondName(std::string str) {m_second_name = str;}
+	void chaneDateOfBirth(std::string str) {m_birth = encode(str);}
+	void printPerson();
 
 protected:
     std::string m_first_name;
     std::string m_second_name;
-    //time_t m_birth;
-   // time_t m_created;
+    time_t m_birth;
 
 
 
